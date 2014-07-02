a6acb4b30582c729ac6ca4edf20b056309207740
jnuwan2011 <jnuwan2011@gmail.com>
2014-07-02 18:48:33 +0000

    Create Privilege.java

---
+++ src/com/hotel/entity/Privilege.java
@@ -0,0 +1,47 @@
+package com.hotel.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="privilege")
+public class Privilege {
+
+	private int privilegeId;
+	private Menu menu;
+	private String privilegeType;
+	
+	@Id
+	@GeneratedValue
+	@Column(name="privilegeId")
+	public int getPrivilegeId() {
+		return privilegeId;
+	}
+	public void setPrivilegeId(int privilegeId) {
+		this.privilegeId = privilegeId;
+	}
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name = "menuId", nullable =false)
+	public Menu getMenu() {
+		return menu;
+	}
+	public void setMenu(Menu menu) {
+		this.menu = menu;
+	}
+	
+	@Column(name="privilegeType")
+	public String getPrivilegeType() {
+		return privilegeType;
+	}
+	public void setPrivilegeType(String privilegeType) {
+		this.privilegeType = privilegeType;
+	}
+	
+}
