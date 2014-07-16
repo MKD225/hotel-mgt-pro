f009d3fb77d1aab4c7816521418f2a612196cef7
jnuwan2011@gmail.com <jnuwan2011@gmail.com@cba416c2-dcec-1889-0d87-aaf77edbd8d1>
2014-07-16 10:29:19 +0000

    change p

--- trunk/src/com/hotel/entity/Privilege.java
+++ trunk/src/com/hotel/entity/Privilege.java
@@ -10,38 +10,41 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="privilege")
+@Table(name = "privilege")
 public class Privilege {
 
 	private int privilegeId;
 	private Menu menu;
 	private String privilegeType;
-	
+
 	@Id
 	@GeneratedValue
-	@Column(name="privilegeId")
+	@Column(name = "privilegeId")
 	public int getPrivilegeId() {
 		return privilegeId;
 	}
+
 	public void setPrivilegeId(int privilegeId) {
 		this.privilegeId = privilegeId;
 	}
-	
-	@ManyToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name = "menuId", nullable =false)
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "menuId", nullable = false)
 	public Menu getMenu() {
 		return menu;
 	}
+
 	public void setMenu(Menu menu) {
 		this.menu = menu;
 	}
-	
-	@Column(name="privilegeType")
+
+	@Column(name = "privilegeType")
 	public String getPrivilegeType() {
 		return privilegeType;
 	}
+
 	public void setPrivilegeType(String privilegeType) {
 		this.privilegeType = privilegeType;
 	}
-	
+
 }
