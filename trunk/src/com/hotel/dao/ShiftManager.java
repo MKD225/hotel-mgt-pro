4f1b1a7913bc1163ef54f11d612e8ffd264c9548
jnuwan2011@gmail.com <jnuwan2011@gmail.com@cba416c2-dcec-1889-0d87-aaf77edbd8d1>
2014-07-16 10:23:03 +0000

    change ShiftManager.java

--- trunk/src/com/hotel/dao/ShiftManager.java
+++ trunk/src/com/hotel/dao/ShiftManager.java
@@ -1,6 +1,6 @@
 package com.hotel.dao;
 
-	import java.util.List;
+import java.util.List;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -10,59 +10,65 @@ import com.hotel.util.HibernateUtil;
 
 public class ShiftManager {
 
-	
-
-	
-
-		public Shift saveOrUpdate(Shift shift) throws Exception{
-			// TODO Auto-generated method stub
-			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-			session.beginTransaction();
-			try {
-				session.saveOrUpdate(shift);
-				session.getTransaction().commit();
-			} catch (HibernateException e) {
-				e.printStackTrace();
-				session.getTransaction().rollback();
-				throw e;
-			}
-			return shift;
-		}
-
-		@SuppressWarnings("unchecked")
-		public List<Shift> search() throws Exception {
-			// TODO Auto-generated method stub
-			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-			session.beginTransaction();
-			List<Shift> shifts = null;
-			try {
-				shifts = (List<Shift>) session.createQuery("from Shift").list();
-			} catch (HibernateException e) {
-				e.printStackTrace();
-				session.getTransaction().rollback();
-				throw e;
-			}
+	public Shift saveOrUpdate(Shift shift) throws Exception {
+		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		try {
+			session.saveOrUpdate(shift);
 			session.getTransaction().commit();
-			return shifts;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			throw e;
 		}
-		public Shift delete(Shift shift) {
-			// TODO Auto-generated method stub
-			
-			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-			session.beginTransaction();
-			try {
-				System.out.print("------delete manager***************");
-				session.delete(shift);
-				session.getTransaction().commit();
-			} catch (HibernateException e) {
-				e.printStackTrace();
-				session.getTransaction().rollback();
-				throw e;
-			}
-			return shift;
-		}
-
-		
+		return shift;
 	}
 
+	@SuppressWarnings("unchecked")
+	public List<Shift> search() throws Exception {
+		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		List<Shift> shifts = null;
+		try {
+			shifts = (List<Shift>) session.createQuery("from Shift").list();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			throw e;
+		}
+		session.getTransaction().commit();
+		return shifts;
+	}
 
+	public Shift delete(Shift shift) {
+		// TODO Auto-generated method stub
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		try {
+			System.out.print("------delete manager***************");
+			session.delete(shift);
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			throw e;
+		}
+		return shift;
+	}
+
+	public Shift getShiftById(int id) {
+		System.out.println("in dao");
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		System.out.println("**dao***");
+		Shift employee = (Shift) session.get(Shift.class, id);
+		System.out.println(employee.getShiftName());
+		session.getTransaction().commit();
+		return employee;
+
+	}
+
+}
